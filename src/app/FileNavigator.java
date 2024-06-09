a4e335b21e9408d962e3ec035c8f5dc6c15d1cae
michelle <gmichele498@gmail.com>
2024-06-09 18:30:39 -0300

    starting project

---
+++ src/app/FileNavigator.java
@@ -0,0 +1,13 @@
+package app;
+
+public class FileNavigator {
+	
+	public static void main(String[] args) {
+		new FileNavigator();
+	}
+
+	public FileNavigator() {
+		
+	}
+	
+}//class FileNavigator
